873edd3b8f44b840d36f393f7cd53b002c5d2aaf
Andrew Vaughan <hello@andrewvaughan.io>
2016-04-29 22:08:52 -0500

    New: Added logging controls for the system (fixes #13)

--- dashpi/__main__.py
+++ dashpi/__main__.py
@@ -4,8 +4,11 @@
 """DashPi the web dashboard controller for Raspberry Pi"""
 
 
+import logging
+import os
 import subprocess
 import sys
+import traceback
 
 import argparse
 import selenium
@@ -17,7 +20,11 @@ from yaml import load as loadconfig
 def main(args):
     """Launches DashPi"""
     
+    logger = logging.getLogger('DashPi')
+    
     # Load the user's configuration file
+    logger.debug('Loading configuration from "%s"', args.config)
+    
     try:
         with open(args.config, 'r') as yml:
             config = loadconfig(yml)
@@ -25,13 +32,19 @@ def main(args):
     except IOError:
         raise SystemExit('The configuration file "%s" could not be found' % args.config)
     
+    logger.debug('Loaded.')
+    
     
     # Ensure the user has set a browser
     if not config or not config['browser'] or config['browser'] not in ['firefox', 'iceweasel', 'chrome', 'opera']:
         raise SystemExit('Configuration option "browser" must be one of: firefox, iceweasel, chrome, ie, opera')
     
     
+    logger.debug('Browser set to "%s"', config['browser'])
+    
+    
     # Check if the browser is already running
+    logger.debug('Checking if browser currently running...')
     try:
         process = subprocess.check_output(["pgrep", config['browser']])
     
@@ -41,11 +54,11 @@ def main(args):
             )
         
     except subprocess.CalledProcessError:
-        pass
+        logger.debug('Browser not found.')
     
     
     # Launch the browser
-    print "Launching %s..." % config['browser']
+    logger.info("Launching %s...", config['browser'])
     
     if config['browser'] == 'chrome':
         browser = webdriver.Chrome()
@@ -58,6 +71,8 @@ def main(args):
     
     
     # Load a test URL
+    logger.debug("Loading URL int browser")
+    
     browser.get('https://andrewvaughan.io/')
 
 
@@ -70,16 +85,84 @@ if __name__ == "__main__":
     PARSER.add_argument('-c', '--config', default='~/dashpi.yml',
                         help="configuration file (default: ~/dashpi.yml)")
     
+    PARSER.add_argument('-q', '--quiet', action='store_true',
+                        help='prevents output to stdout')
+    
+    PARSER.add_argument('-v', '--verbose', action='count', default=0,
+                        help='verbosity level for logging (-vv for max verbosity)')
+    
+    PARSER.add_argument('-l', '--log',
+                        help='the log file to write to')
+    
+    ARGS = PARSER.parse_args()
+    
+    
+    # Setup the logger
+    LOGGER = logging.getLogger()
+    
+    LOGGER.setLevel(logging.WARNING)
+    
+    if hasattr(ARGS, 'verbose'):
+        if ARGS.verbose == 1:
+            LOGGER.setLevel(logging.INFO)
+        
+        elif ARGS.verbose == 2:
+            LOGGER.setLevel(logging.DEBUG)
+        
+        elif ARGS.verbose > 2:
+            LOGGER.setLevel(1)
+    
+    
+    # Setup console logging
+    SH_CONSOLE = logging.StreamHandler(sys.stdout)
+
+    SH_CONSOLE.setFormatter(
+        logging.Formatter(
+            fmt='%(name)-12s :: %(levelname)-8s : %(message)s'
+        )
+    )
+
+    if ARGS.quiet:
+        sys.stdout = os.devnull
+        SH_CONSOLE.setLevel(9999)
+
+    LOGGER.addHandler(SH_CONSOLE)
+
+
+    # Setup file logging
+    if ARGS.log != None:
+        SH_FILE = logging.FileHandler(ARGS.log)
+
+        ARGS.setFormatter(
+            logging.Formatter(
+                fmt='[%(asctime)s] %(name)-12s :: %(levelname)-8s : %(message)s',
+                datefmt='%m-%d-%Y %H:%M:%S'
+            )
+        )
+
+        LOGGER.addHandler(SH_FILE)
+    
+    
+    # Send all exceptions through the logger
+    def handle_exception(ex_cls, ex, trace):
+        """Catches all errors and sends them through the logger"""
+
+        LOGGER.critical("EXCEPTION")
+        LOGGER.critical(''.join(traceback.format_tb(trace)))
+        LOGGER.critical('%s: %s', ex_cls, ex)
+
+    sys.excepthook = handle_exception
+    
     
     # Launch and exit with the proper code
     try:
-        main(PARSER.parse_args())
+        main(ARGS)
         
     except SystemExit, err:
-        print "Error: %s" % str(err)
+        LOGGER.error(str(err))
         sys.exit(1)
     
     except selenium.common.exceptions.WebDriverException, err:
-        print "DashPi could not connect to the web browser:"
-        print str(err)
+        LOGGER.error("DashPi could not connect to the web browser.")
+        LOGGER.error(str(err))
         sys.exit(2)
